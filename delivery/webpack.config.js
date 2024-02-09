3fd6e8c094762e1cc916f0f9a3611f2a5788d9ef
Ashwani Yadav <ashwani.yadav@ezeiatech.in>
2024-02-09 13:03:56 +0530

    changes the build of delivery

--- delivery/webpack.config.js
+++ delivery/webpack.config.js
@@ -1,79 +1,66 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
 const deps = require("./package.json").dependencies;
+module.exports = (_, argv) => ({
+  output: {
+    publicPath: "http://localhost:9003/",
+  },
 
-module.exports = (_, argv) => {
-  // Determine the mode (development or production)
-  const isProduction = argv.mode === 'production';
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
 
-  // Set publicPath dynamically based on the environment
-  const publicPath = isProduction ? "https://yourproductiondomain.com/" : "http://localhost:9003/";
+  devServer: {
+    port: 9003,
+    historyApiFallback: true,
+  },
 
-  return {
-    output: {
-      publicPath: publicPath,
-      filename: isProduction ? '[name].[contenthash].js' : '[name].bundle.js',
-    },
-
-    resolve: {
-      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-    },
-
-    devServer: {
-      port: 9003,
-      historyApiFallback: true,
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.m?js/,
-          type: "javascript/auto",
-          resolve: {
-            fullySpecified: false,
-          },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
         },
-        {
-          test: /\.(css|s[ac]ss)$/i,
-          use: ["style-loader", "css-loader", "sass-loader"],
-        },
-        {
-          test: /\.(ts|tsx|js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
-        },
-      ],
-    },
-
-    plugins: [
-      new ModuleFederationPlugin({
-        name: "delivery",
-        filename: "delivery-app.js",
-        remotes: {},
-        exposes: {
-          "./DeliveryApp": "./src/App.jsx",
-        },
-        shared: {
-          ...deps,
-          react: { singleton: true, eager: true, requiredVersion: deps.react },
-          "react-dom": { singleton: true, eager: true, requiredVersion: deps["react-dom"] },
-        },
-      }),
-      new HtmlWebPackPlugin({
-        template: "./src/index.html",
-      }),
-    ],
-
-    // Enable source maps in development for better debugging
-    devtool: isProduction ? false : 'source-map',
-
-    // Optimization settings for production
-    optimization: isProduction ? {
-      splitChunks: {
-        chunks: 'all',
       },
-    } : {},
-  };
-};
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "delivery",
+      filename: "delivery-app.js",
+      remotes: {},
+      exposes: {
+        "./DeliveryApp": "./src/App.jsx",
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+});
